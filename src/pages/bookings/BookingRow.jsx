0b236bcf872b326fc89c6cd05ca7f3c1e3d6b4fa
monir3355 <16173103036@cse.bubt.edu.bd>
2023-05-12 17:46:29 +0600

    deleted from ui and remaing update

--- src/pages/bookings/BookingRow.jsx
+++ src/pages/bookings/BookingRow.jsx
@@ -1,13 +1,29 @@
 import React from "react";
 
-const BookingRow = ({ booking }) => {
-  const { amount, title, date, img, customerName } = booking;
+const BookingRow = ({ booking, handleDelete }) => {
+  const { _id, amount, title, date, img, customerName } = booking;
   return (
     <tr>
       <th>
-        <label>
-          <input type="checkbox" className="checkbox" />
-        </label>
+        <button
+          onClick={() => handleDelete(_id)}
+          className="btn btn-circle btn-sm"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M6 18L18 6M6 6l12 12"
+            />
+          </svg>
+        </button>
       </th>
       <td>
         <div className="flex items-center space-x-3">
--- src/pages/bookings/Bookings.jsx
+++ src/pages/bookings/Bookings.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../provider/AuthProvider";
 import BookingRow from "./BookingRow";
+import Swal from "sweetalert2";
 
 const Bookings = () => {
   const { user } = useContext(AuthContext);
@@ -11,6 +12,34 @@ const Bookings = () => {
       .then((res) => res.json())
       .then((data) => setBookings(data));
   }, []);
+
+  const handleDelete = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        // Swal.fire("Deleted!", "Your file has been deleted.", "success");
+        fetch(`http://localhost:5000/bookings/${id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data);
+            if (data.deletedCount > 0) {
+              Swal.fire("Deleted!", "Your file has been deleted.", "success");
+            }
+            const remaining = bookings.filter((booking) => booking._id !== id);
+            setBookings(remaining);
+          });
+      }
+    });
+  };
   return (
     <div>
       <h2>My bookings : {bookings.length}</h2>
@@ -19,7 +48,11 @@ const Bookings = () => {
           <tbody>
             {/* row 1 */}
             {bookings.map((booking) => (
-              <BookingRow key={booking._id} booking={booking}></BookingRow>
+              <BookingRow
+                key={booking._id}
+                booking={booking}
+                handleDelete={handleDelete}
+              ></BookingRow>
             ))}
             {/* row 2 */}
           </tbody>
